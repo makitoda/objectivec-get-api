891a39577bce8e3ede8cb442e74773676c762b87
maki toda <earth.in.us@gmail.com>
2017-03-24 11:26:10 -0700

    show the result of json on console.

--- Sample-GetAPI/ViewController.m
+++ Sample-GetAPI/ViewController.m
@@ -15,14 +15,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Request URL
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:@"http://sonarsystems.co.uk/DeveloperTools/Tutorials/iOS_SDK/ReturnJSONArray.php?arrayType=Array3"]];
+    
+    NSURLSessionDataTask *task = [[self getURLSession] dataTaskWithRequest:request
+                                                         completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                             dispatch_async(dispatch_get_main_queue(),
+                                                                            ^{
+                                                                                // parse returned JSON array
+                                                                                NSError *jsonError;
+                                                                                NSArray *parsedJSONArray = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&jsonError];
+                                                                                
+                                                                                NSLog(@"%@", parsedJSONArray);
+                                                                            });
+                                                         }];
+    
+    [task resume];
 }
 
+- (NSURLSession *)getURLSession {
+    static NSURLSession *session = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken,
+                  ^{
+                      NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+                      session = [NSURLSession sessionWithConfiguration:configuration];
+                  });
+    
+    return session;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
 
 @end
